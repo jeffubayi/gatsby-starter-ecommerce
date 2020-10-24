d46a69fef10e5bcd1bc29e5cd044cd689e901a90
ubeezy <jeffubayi@gmail.com>
2020-10-24 13:45:40 +0300

    Update DesktopMenu.js

--- src/components/Header/DesktopMenu.js
+++ src/components/Header/DesktopMenu.js
@@ -21,7 +21,7 @@ const DesktopMenu = ({location: {pathname}, token, cartCount, signout}) => {
           header
         >
           <Logo />
-          Starter Store
+          Chic Kloset
         </Menu.Item>
         {token ? (
           <Menu.Menu position="right">
